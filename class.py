bfd2deabaab98979f7d5dcce037660e609352bfe
Samantha WathenOlson <samantha.wathen269@gmail.com>
2022-02-23 16:00:38 -0500

    Create class.py

---
+++ class.py
@@ -0,0 +1,47 @@
+"""These exercises are designed to familiarize you with classes and the ways you can utilize them"""
+
+"""
+Create a class whose objects have string, int, and boolean fields when instantiated
+"""
+class_MySpecialClass:
+  
+
+def __init__(self, my_string_var: str, my_integer_var: int, my_boolean: str_boolean)
+  self.my_string_variable = my_string_variable
+  self.my_integer_variable = my_integer_variable
+  self.my_boolean = my_boolean
+  
+"""
+Create a class that has a string, int, and boolean class field
+"""
+class SecondClass:
+  def __init__(self, MySpecialClass):
+    self.MySpecialClass = MySpecialClass
+    print("")
+    
+
+"""
+Create a class whose objects have two int fields and create a single method for each field that
+returns its value
+"""
+class ThirdClass:
+  def __init__(self, input_one: int, input_two:int)
+  self.input_one = 
+
+"""
+Create a class whose objects have two string fields and a method to get the value of each field. Create a method
+that performs string concatenation on the two fields and prints the result to the terminal. You may not directly
+access the fields for the method that performs the string concatenation
+"""
+
+
+"""
+Create an abstract class that has an int field called object_count that is set at zero. Then create a class that 
+inherits from the abstract class and increases the value of object_count by one whenever the class is instantiated
+"""
+
+
+"""
+Create an abstract class with an abstract method. Create a second class that inherits from the abstract class that
+implements the abstract method. The implemented method must print a message to the terminal
+"""
